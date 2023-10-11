828d1d73dd95ee6e38d7a5cc4fb7f003360a83bc
José Antonio Fernández <syntaxter@outlook.com>
2023-10-11 06:20:24 -0500

    Add > Desorro de las diferentes secciones de portal b2b

---
+++ b2b-prototipo/main.js
@@ -0,0 +1,498 @@
+
+// Variables globales
+let preciosInsumos = 
+    [
+      {
+        "codigo": "I007573",
+        "precio": "39724",
+        "descripcion": "ESPUMA EX ROSADA D26 100X200X3",
+        "imageEsponja":"https://res.cloudinary.com/www-ismyt-com/image/upload/v1696971826/IMAGENES/JAMAR/B2B/qh4cyfvvh3onqvk4athn.png"
+      },
+      {
+        "codigo": "9003218",
+        "precio": "46704",
+        "descripcion": "ESPUMA EX ROSADA D26 100X200X5",
+        "imageEsponja":"https://res.cloudinary.com/www-ismyt-com/image/upload/v1696971826/IMAGENES/JAMAR/B2B/qh4cyfvvh3onqvk4athn.png"
+        
+    },
+    {
+        "codigo": "I083122",
+        "precio": "39724",
+        "descripcion": "ESPUMA PENTA 26 GRIS 100X200X3",
+        "imageEsponja":"https://res.cloudinary.com/www-ismyt-com/image/upload/v1696971826/IMAGENES/JAMAR/B2B/ws4arxhgkqcoa194kjbr.png"
+    },
+    {
+        "codigo": "9003211",
+        "precio": "43368",
+        "descripcion": "ESPUMA PENTA 26 GRIS 100X200X05",
+        "imageEsponja":"https://res.cloudinary.com/www-ismyt-com/image/upload/v1696971826/IMAGENES/JAMAR/B2B/ws4arxhgkqcoa194kjbr.png"
+    },
+    {
+        "codigo": "I007570",
+        "precio": "25036",
+        "descripcion": "ESPUMA SF AZUL D18 100X200X3",
+        "imageEsponja":"https://res.cloudinary.com/www-ismyt-com/image/upload/v1696971826/IMAGENES/JAMAR/B2B/xkaxozpnayen931nzlll.png"
+      },
+      {
+        "codigo": "9003210",
+        "precio": "33805",
+        "descripcion": "ESPUMA SF AZUL D18 100X200X5",
+        "imageEsponja":"https://res.cloudinary.com/www-ismyt-com/image/upload/v1696971826/IMAGENES/JAMAR/B2B/xkaxozpnayen931nzlll.png"
+    },
+    {
+        "codigo": "I007579",
+        "precio": "5606",
+        "descripcion": "ESPUMA TSE AMARILLA D15 100X200X1",
+        "imageEsponja":"https://res.cloudinary.com/www-ismyt-com/image/upload/v1696971827/IMAGENES/JAMAR/B2B/qxcwzbrxgv9wc9eba1ak.png"
+      },
+      {
+        "codigo": "I007578",
+        "precio": "11211",
+        "descripcion": "ESPUMA TSE AMARILLA D15 100X200X2",
+        "imageEsponja":"https://res.cloudinary.com/www-ismyt-com/image/upload/v1696971827/IMAGENES/JAMAR/B2B/qxcwzbrxgv9wc9eba1ak.png"
+      }
+]
+
+  let espumas = [
+    {
+        "sku": "7026484",
+        "material": "I007573",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7023395",
+        "material": "I007573",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7024699",
+        "material": "I007573",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7024132",
+        "material": "I007573",
+        "unidad": "1.00"
+    },
+    {
+        "sku": "7024076",
+        "material": "I007573",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7026052",
+        "material": "I007573",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7020529",
+        "material": "I007573",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7023980",
+        "material": "I007573",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7024116",
+        "material": "I007573",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7026484",
+        "material": "9003218",
+        "unidad": "0.20"
+    },
+    {
+        "sku": "7023395",
+        "material": "9003218",
+        "unidad": "1.63"
+    },
+    {
+        "sku": "7024699",
+        "material": "9003218",
+        "unidad": "1.63"
+    },
+    {
+        "sku": "7024132",
+        "material": "9003218",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7024076",
+        "material": "9003218",
+        "unidad": "1.30"
+    },
+    {
+        "sku": "7026052",
+        "material": "9003218",
+        "unidad": "0.82"
+    },
+    {
+        "sku": "7020529",
+        "material": "9003218",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7023980",
+        "material": "9003218",
+        "unidad": "1.63"
+    },
+    {
+        "sku": "7024116",
+        "material": "9003218",
+        "unidad": "0.16"
+    },
+    {
+        "sku": "7026484",
+        "material": "I083122",
+        "unidad": "0.20"
+    },
+    {
+        "sku": "7023395",
+        "material": "I083122",
+        "unidad": "1.60"
+    },
+    {
+        "sku": "7024699",
+        "material": "I083122",
+        "unidad": "1.60"
+    },
+    {
+        "sku": "7024132",
+        "material": "I083122",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7024076",
+        "material": "I083122",
+        "unidad": "1.30"
+    },
+    {
+        "sku": "7026052",
+        "material": "I083122",
+        "unidad": "1.80"
+    },
+    {
+        "sku": "7020529",
+        "material": "I083122",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7023980",
+        "material": "I083122",
+        "unidad": "1.60"
+    },
+    {
+        "sku": "7024116",
+        "material": "I083122",
+        "unidad": "0.48"
+    },
+    {
+        "sku": "7026484",
+        "material": "9003211",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7023395",
+        "material": "9003211",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7024699",
+        "material": "9003211",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7024132",
+        "material": "9003211",
+        "unidad": "1.00"
+    },
+    {
+        "sku": "7024076",
+        "material": "9003211",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7026052",
+        "material": "9003211",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7020529",
+        "material": "9003211",
+        "unidad": "2.60"
+    },
+    {
+        "sku": "7023980",
+        "material": "9003211",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7024116",
+        "material": "9003211",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7026484",
+        "material": "I007570",
+        "unidad": "0.50"
+    },
+    {
+        "sku": "7023395",
+        "material": "I007570",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7024699",
+        "material": "I007570",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7024132",
+        "material": "I007570",
+        "unidad": "0.80"
+    },
+    {
+        "sku": "7024076",
+        "material": "I007570",
+        "unidad": "0.75"
+    },
+    {
+        "sku": "7026052",
+        "material": "I007570",
+        "unidad": "0.96"
+    },
+    {
+        "sku": "7020529",
+        "material": "I007570",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7023980",
+        "material": "I007570",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7024116",
+        "material": "I007570",
+        "unidad": "0.03"
+    },
+    {
+        "sku": "7026484",
+        "material": "9003210",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7023395",
+        "material": "9003210",
+        "unidad": "1.20"
+    },
+    {
+        "sku": "7024699",
+        "material": "9003210",
+        "unidad": "1.20"
+    },
+    {
+        "sku": "7024132",
+        "material": "9003210",
+        "unidad": "0.07"
+    },
+    {
+        "sku": "7024076",
+        "material": "9003210",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7026052",
+        "material": "9003210",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7020529",
+        "material": "9003210",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7023980",
+        "material": "9003210",
+        "unidad": "1.20"
+    },
+    {
+        "sku": "7024116",
+        "material": "9003210",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7026484",
+        "material": "I007579",
+        "unidad": "0.20"
+    },
+    {
+        "sku": "7023395",
+        "material": "I007579",
+        "unidad": "3.33"
+    },
+    {
+        "sku": "7024699",
+        "material": "I007579",
+        "unidad": "3.33"
+    },
+    {
+        "sku": "7024132",
+        "material": "I007579",
+        "unidad": "1.50"
+    },
+    {
+        "sku": "7024076",
+        "material": "I007579",
+        "unidad": "3.90"
+    },
+    {
+        "sku": "7026052",
+        "material": "I007579",
+        "unidad": "2.50"
+    },
+    {
+        "sku": "7020529",
+        "material": "I007579",
+        "unidad": "1.30"
+    },
+    {
+        "sku": "7023980",
+        "material": "I007579",
+        "unidad": "3.33"
+    },
+    {
+        "sku": "7024116",
+        "material": "I007579",
+        "unidad": "1.00"
+    },
+    {
+        "sku": "7026484",
+        "material": "I007578",
+        "unidad": "0.00"
+    },
+    {
+        "sku": "7023395",
+        "material": "I007578",
+        "unidad": "0.50"
+    },
+    {
+        "sku": "7024699",
+        "material": "I007578",
+        "unidad": "0.50"
+    },
+    {
+        "sku": "7024132",
+        "material": "I007578",
+        "unidad": "1.20"
+    },
+    {
+        "sku": "7024076",
+        "material": "I007578",
+        "unidad": "0.16"
+    },
+    {
+        "sku": "7026052",
+        "material": "I007578",
+        "unidad": "0.60"
+    },
+    {
+        "sku": "7020529",
+        "material": "I007578",
+        "unidad": "0.60"
+    },
+    {
+        "sku": "7023980",
+        "material": "I007578",
+        "unidad": "0.50"
+    },
+    {
+        "sku": "7024116",
+        "material": "I007578",
+        "unidad": "0.00"
+    }
+]
+
+
+window.addEventListener("click", (e) => {
+    e.preventDefault()
+    let productName = e.target.dataset.productName;
+
+    if (e.target.classList.contains("card-product-page-list")) {
+        let url = new URL(window.location.href);
+        url.searchParams.set("product", e.target.dataset.urlProduct);
+    
+        history.pushState(null, null, url.toString())
+        
+        renderProductPage(productName)
+    }
+})
+
+
+function renderProductPage(product){
+    let renderProductsContainer = document.querySelector(".render-components");
+    let objetoMateriales = preciosInsumos;
+    renderProductsContainer.innerHTML = `
+    <div class="row">
+    <div class="col-5">
+        <img class="img-fluid w-100" src="https://d3lome5o0h180x.cloudfront.net/eyJidWNrZXQiOiJiYWNrYm9uZS1hc3NldHMtcHJkIiwia2V5IjoiQVNUXzI4MDI5OC9BU1RfMjgwMjk4LmpwZyIsImVkaXRzIjp7InJlc2l6ZSI6eyJ3aWR0aCI6MTUwMCwiaGVpZ2h0IjoxNTAwLCJmaXQiOiJjb250YWluIn19fQ==" alt="">
+    </div>
+    <div class="col-7">
+        <div class="container-materiales border h-100 rounded p-4">
+            <div class="row">
+                <h1 class="fs-4 text-dark">${product}</h1>
+            </div>
+            <div class="row mt-3 render-materiales gap-2">
+            </div>
+        </div>
+    </div>
+</div>`;
+//renderizando los elementos internos
+renderMateriales(objetoMateriales)
+
+}
+
+
+function renderMateriales(objetoMateriales) {
+    let renderMaterialesList = document.querySelector(".render-materiales")
+    const elementosFiltrados = espumas.filter(espumas => espumas.sku == 7026484);
+    
+
+    elementosFiltrados.forEach(element => {
+        //verificar el nombre de la espuma
+        console.log(element)
+        let espumasFiltradas = preciosInsumos.filter(preciosInsumos => preciosInsumos.codigo == element.material);
+        console.log(espumasFiltradas)
+
+
+        if (element.unidad > 0) {
+            renderMaterialesList.innerHTML += `
+            <div class="card" style="width: 7rem;">
+            <ul class="list-group list-group-flush d-flex justify-content-center text-center">
+                <img src="${espumasFiltradas[0].imageEsponja}" />
+                <h5 class="text-start material-text-size mt-2"> ${espumasFiltradas[0].descripcion} </h5>
+                <h5 class="text-start material-text-size"> ${element.unidad} </h5>
+              <li class="list-group-item">
+              <div class="d-flex justify-content-center align-items-center">
+                <button class="btn"><i class="bi bi-dash-circle-fill"></i></button>
+                <span>0</span>
+                <button class="btn"><i class="bi bi-plus-circle-fill"></i></button>
+              </div>
+              </li>
+            </ul>
+          </div>
+        `
+    }});
+}
+
+
+
+
+
+
+
+
+
